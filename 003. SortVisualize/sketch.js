379cfb6a995cb37e023936e01073241420f76a23
Kamil Kozioł <38388156+kamil-koziol@users.noreply.github.com>
2020-02-27 23:26:36 +0100

    poprawki

--- 003. SortVisualize/sketch.js
+++ 003. SortVisualize/sketch.js
@@ -1,17 +1,28 @@
 var arr = [];
 var minimal;
 var maximal;
+var items = 100;
+var counter = 0;
 
 function setup() {
   createCanvas(800, 800);
-  for(var i=0; i<50; i++) {
-    arr.push(floor(random(1,11)));
+  
+  for(var i=0; i<items; i++) {
+    arr.push(i);
   }
-
-  minimal = Math.min.apply(Math, arr);
-  maximal = Math.max.apply(Math, arr);
+  background(0);
+  minimal = 0
+  maximal = items-1;
   drawArray();
-  bubbleSort(arr, 1);
+}
+
+function mousePressed() {
+  counter++;
+  if(counter == 1) {
+    randomizeArray(arr)
+  } else if(counter == 2) {
+    bubbleSort(arr,1);
+  }
 }
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -25,11 +36,11 @@ async function bubbleSort(arr, refreshRate) {
         var tmp = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = tmp;
-
+        
         // purplish rect
         fill(100,0,255);
         var sx = map(j, 0, arr.length-1, 0, width-width/arr.length);
-        var sy = map(arr[j+1], minimal, maximal, height/10,height-5);
+        var sy = map(arr[j+1], minimal, maximal, 0,height-5);
         rect(sx, height, width/arr.length, -sy);
         await sleep(refreshRate);
         background(0);
@@ -43,11 +54,25 @@ function drawArray() {
   for(var i=0; i<arr.length; i++) {
     fill(255);
     var sx = map(i, 0, arr.length-1, 0, width-width/arr.length);
-    var sy = map(arr[i], minimal, maximal, height/10,height-5);
+    var sy = map(arr[i], minimal, maximal, 0,height-5);
     rect(sx, height, width/arr.length, -sy);
   }
 }
+async function randomizeArray(arr) {
+  for(var i=0; i<arr.length*3; i++) {
+    random_x = floor(random(arr.length))
+    random_y = floor(random(arr.length))
+    
+    var tmp = arr[random_x];
+    arr[random_x] = arr[random_y];
+    arr[random_y] = tmp;
+    await sleep(1);
+    background(0);
+    drawArray();
+  }
 
+  return true;
+}
 // 10,5,4,3,2
 // 5,10,4,3,2
 // 5,4,10,3,2
