355ea3b6ec9dce6b8840e0de64b8658f8ffba6c9
Kamil Kozioł <38388156+kamil-koziol@users.noreply.github.com>
2020-02-27 16:33:18 +0100

    Dodano StarField!

---
+++ 001. StarField/sketch.js
@@ -0,0 +1,17 @@
+var stars = [];
+
+function setup() {
+  createCanvas(800, 800);
+  for (let i = 0; i < 100; i++) {
+    stars.push(new Star());
+  }
+}
+
+function draw() {
+  background(0);
+  translate(width/2, height/2);
+    for(let star of stars) {
+      star.show();
+      star.update();
+  }
+}
---
+++ 001. StarField/star.js
@@ -0,0 +1,24 @@
+class Star {
+    constructor() {
+        this.x = random(-width/2,width/2);
+        this.y = random(-height/2,height/2);
+        this.z = width;
+        this.speed = random(4,10);
+    }
+
+    update() {
+        this.z -= this.speed;
+        if (this.z < 1) {
+            this.z = width;
+        }
+    }
+
+    show() {
+        var sx = map(this.x / this.z, -1, 1, -width/2, width/2);
+        var sy = map(this.y / this.z, -1, 1, -height/2, height/2);
+        var sr = map(this.z, 0, width, 16, 0);
+        noStroke();
+        fill(255);
+        ellipse(sx, sy, sr);
+    }
+}
