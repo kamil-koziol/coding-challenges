19b9159675a95b07f91d72d6a072fbc7a5475931
Kamil Kozioł <38388156+kamil-koziol@users.noreply.github.com>
2020-03-02 13:40:22 +0100

    Dodano phyllotaxis
    Z kolorowaniem

---
+++ 009. Phyllotaxis/sketch.js
@@ -0,0 +1,23 @@
+var angle = 137.5;
+var c = 15;
+var n = 0;
+
+function setup() {
+  createCanvas(800, 800);
+  background(0);
+}
+
+function draw() {
+  translate(width / 2, height / 2);
+  var r = c * sqrt(n);
+  var theta = n * angle;
+  var x = r * cos(theta);
+  var y = r * sin(theta);
+
+  var colr = map(x, 0, width/2, 50,150);
+  var colg = map(y,0, height,100, 150);
+  fill(colr, 50, colg);
+  ellipse(x, y, 10, 10);
+
+  n+=1;
+}
